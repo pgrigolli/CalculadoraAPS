860b14b0f23cd78fe89fe2c753d0514bc96a46c0
pgrigolli <141965505+pgrigolli@users.noreply.github.com>
2023-09-14 16:38:54 -0300

    Add files via upload

---
+++ botao.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+class botao{
+    public:
+        char simbolo;
+}
---
+++ tela.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+class tela{
+    private:
+        int visor:
+
+    public:
+        int add();
+        int reset();
+        int get();
+
+}
